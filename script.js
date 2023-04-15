5233dce5432a2cb0d74ce6bb8ab227b172515f0a
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-15 15:44:54 +0200

    The message now will select both divs by their class and then to assign the textContent to each one.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function checkCoincidence (event) {
     event.preventDefault();
     let password = document.getElementById('password').value;
     let repeatPassword = document.getElementById('repeat-password').value;
-    let message = document.getElementsByClassName('message')[0];
+    let message = document.getElementsByClassName('message');
     if (password.length != 0) {
         if (password === repeatPassword) {
                 document.getElementById('form').submit();
         } else {
-                message.textContent = 'Passwords do not match!';
+                message[0].textContent = 'Passwords do not match!';
+                message[1].textContent = 'Passwords do not match!';
         }
     }
     console.log(message.textContent);
