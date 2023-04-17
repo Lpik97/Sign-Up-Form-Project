0f1e34a665cf47e98884cf3ce99176fae198e12d
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-17 09:09:44 +0200

    The function checkCoincidence will select the password, repeatPassword and message to check if wether the passwords match, and if they dont the form will not be submitted and two error messages will pop up to notify the user that the passwords does not match. An event listener will be waiting for the user to click the submit button having the function as a callback.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function checkCoincidence (event) {
-    event.preventDefault();
+function checkCoincidence(e) {
     let password = document.getElementById('password').value;
     let repeatPassword = document.getElementById('repeat-password').value;
     let message = document.getElementsByClassName('message');
-    if (password.length != 0) {
-        if (password === repeatPassword) {
-                document.getElementById('form').submit();
-        } else {
-                message[0].textContent = 'Passwords do not match!';
-                message[1].textContent = 'Passwords do not match!';
-        }
+
+    if (password !== repeatPassword) {
+        e.preventDefault(); // Prevent form submission
+        message[0].textContent = 'Passwords do not match!';
+        message[1].textContent = 'Passwords do not match!';
+    } else {
+        message[0].textContent = '';
+        message[1].textContent = '';
     }
-    console.log(message.textContent);
 }
+
+document.getElementById('form').addEventListener('submit', checkCoincidence);
