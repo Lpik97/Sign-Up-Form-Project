5a0c55f5e9667b3711096be50e7c9918e4fe6665
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-17 09:31:07 +0200

    A password input was selected using querySelectorAll. Then that password input is called when the passwords do not match, assigning a class of wrong-password for each password input  and doing the opposite when the passwords do match.

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ function checkCoincidence(e) {
     let password = document.getElementById('password').value;
     let repeatPassword = document.getElementById('repeat-password').value;
     let message = document.getElementsByClassName('message');
+    let passwordInput = document.querySelectorAll('input[type="password"]');
+
 
     if (password !== repeatPassword) {
         e.preventDefault(); // Prevent form submission
         message[0].textContent = 'Passwords do not match!';
         message[1].textContent = 'Passwords do not match!';
+        passwordInput.forEach(password => {
+            password.classList.add('wrong-password');
+        });
     } else {
         message[0].textContent = '';
         message[1].textContent = '';
+        passwordInput.forEach(input => {
+            input.classList.remove('wrong-password');
+        });    
     }
 }
 
